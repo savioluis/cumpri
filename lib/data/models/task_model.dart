6be75207977eb102baa10c806f46c4ea1758ee7d
savioluis <savioluislobo@gmail.com>
2025-08-22 13:19:58 -0300

    refactor: update TaskModel with id and copyWith, toString methods

--- lib/data/models/task_model.dart
+++ lib/data/models/task_model.dart
@@ -1,19 +1,48 @@
 class TaskModel {
+  final int? id;
   final String title;
   final String? description;
   final bool isDone;
 
   TaskModel({
+    this.id,
     required this.title,
     this.description,
     this.isDone = false,
   });
 
-  TaskModel copyWith({String? title, String? description, bool? isDone}) {
+  TaskModel copyWith({
+    int? id,
+    String? title,
+    String? description,
+    bool? isDone,
+  }) {
     return TaskModel(
+      id: id ?? this.id,
       title: title ?? this.title,
       description: description ?? this.description,
       isDone: isDone ?? this.isDone,
     );
+  }
+
+  @override
+  String toString() {
+    return 'TaskModel(id: $id, title: "$title", description: "$description", isDone: $isDone)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is TaskModel &&
+        other.id == id &&
+        other.title == title &&
+        other.description == description &&
+        other.isDone == isDone;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^ title.hashCode ^ description.hashCode ^ isDone.hashCode;
   }
 }

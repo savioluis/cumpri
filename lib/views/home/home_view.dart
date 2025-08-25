d045249bbab0bc97832184d2039e6819504fb2af
savioluis <savioluislobo@gmail.com>
2025-08-25 11:53:13 -0300

    refactor: update home view with the state observable from the TaskStore

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
+import 'package:cumpri/core/extensions/color_extension.dart';
 import 'package:cumpri/core/extensions/navigator_extension.dart';
+import 'package:cumpri/core/extensions/text_extension.dart';
 import 'package:cumpri/core/utils/date_time_util.dart';
 import 'package:cumpri/core/utils/dialog_util.dart';
 import 'package:cumpri/core/widgets/cumpri_appbar.dart';
@@ -9,8 +13,8 @@ import 'package:cumpri/views/home/widgets/task_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 
-class HomeView extends StatelessWidget {
-  HomeView({
+class HomeView extends StatefulWidget {
+  const HomeView({
     required this.themeStore,
     required this.taskStore,
     super.key,
@@ -19,68 +23,171 @@ class HomeView extends StatelessWidget {
   final ThemeStore themeStore;
   final TaskStore taskStore;
 
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
   final currentDay = DateTime.now().day;
   final currentMonth = DateTimeUtil.getMonthName(DateTime.now().month);
 
-  List get tasks => taskStore.tasks;
+  PreferredSize? appBar;
+  Widget? floatingActionButton;
+  Widget? body;
+
+  @override
+  void initState() {
+    widget.taskStore.loadInitialTasks();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Observer(
       builder: (context) {
-        return Scaffold(
-          appBar: CumpriAppBar(
+        final state = widget.taskStore.state;
+        log('current state: $state');
+
+        if (state is TaskLoading) {
+          appBar = CumpriAppBar(
+            title: '$currentDay de $currentMonth',
+            actionIcon: widget.themeStore.isDarkMode ? const Icon(Icons.light_mode) : const Icon(Icons.dark_mode),
+            onActionPressed: widget.themeStore.toggleTheme,
+            subtitle: 'Encontrando tarefas...',
+          );
+
+          body = const Center(child: CircularProgressIndicator());
+        }
+
+        if (state is TaskError) {
+          body = Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 12,
+              children: [
+                Text(
+                  'Falha ao carregar os dados: ${state.message}',
+                  style: context.displaySmall,
+                  textAlign: TextAlign.center,
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    await widget.taskStore.loadInitialTasks();
+                  },
+                  child: const Text('Tentar novamente'),
+                ),
+              ],
+            ),
+          );
+        }
+
+        if (state is TaskSuccess) {
+          final tasks = state.tasks;
+
+          appBar = CumpriAppBar(
             title: '$currentDay de $currentMonth',
             subtitle: tasks.isNotEmpty
                 ? tasks.length > 1
                       ? '${tasks.length} tarefas encontradas'
                       : '1 tarefa encontrada'
-                : 'Nenhuma tarefa encontrada',
-            actionIcon: themeStore.isDarkMode ? Icon(Icons.light_mode) : Icon(Icons.dark_mode),
-            onActionPressed: themeStore.toggleTheme,
-          ),
-          body: ListView.separated(
-            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 36),
-            itemCount: taskStore.tasks.length,
-            separatorBuilder: (_, _) => const SizedBox(height: 24),
-            itemBuilder: (context, index) {
-              final task = taskStore.tasks[index];
-              return TaskWidget(
-                title: task.title,
-                description: task.description,
-                isDone: task.isDone,
-                onCheckPressed: () => taskStore.toggleTaskDone(task),
-                onTaskPressed: () => context.pushView(
-                  (context) => TaskFormView(
-                    taskStore: taskStore,
-                    task: task,
-                  ),
-                ),
-                onTaskLongPressed: () => DialogUtil.showAlert(
-                  context: context,
-                  message: 'Deseja mesmo apagar essa atividade e todos os seus dados ?',
-                  title: 'Aviso',
-                  confirmLabel: 'Apagar',
-                  cancelLabel: 'Cancelar',
-                  onConfirm: () {
-                    taskStore.deleteTask(task);
-                    context.popView(context);
+                : null,
+            actionIcon: widget.themeStore.isDarkMode ? const Icon(Icons.light_mode) : const Icon(Icons.dark_mode),
+            onActionPressed: widget.themeStore.toggleTheme,
+          );
+
+          floatingActionButton = tasks.isNotEmpty
+              ? FloatingActionButton(
+                  onPressed: () {
+                    context.pushView(
+                      (context) => TaskFormView(
+                        taskStore: widget.taskStore,
+                      ),
+                    );
                   },
-                  onCancel: () => context.popView(context),
-                ),
-              );
-            },
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              context.pushView(
-                (context) => TaskFormView(
-                  taskStore: taskStore,
-                ),
-              );
-            },
-            child: const Icon(Icons.add),
-          ),
+                  child: const Icon(Icons.add),
+                )
+              : null;
+
+          body = RefreshIndicator(
+            backgroundColor: context.surfaceColor,
+            color: context.onSurfaceColor,
+            onRefresh: () async => await widget.taskStore.loadInitialTasks(),
+            child: tasks.isNotEmpty
+                ? ListView.separated(
+                    padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 36),
+                    itemCount: tasks.length,
+                    separatorBuilder: (_, __) => const SizedBox(height: 24),
+                    itemBuilder: (context, index) {
+                      final task = tasks[index];
+                      return TaskWidget(
+                        title: task.title,
+                        description: task.description,
+                        isDone: task.isDone,
+                        onCheckPressed: () => widget.taskStore.toggleTaskDone(task),
+                        onTaskPressed: () => context.pushView(
+                          (context) => TaskFormView(
+                            taskStore: widget.taskStore,
+                            task: task,
+                          ),
+                        ),
+                        onTaskLongPressed: () => DialogUtil.showAlert(
+                          context: context,
+                          message: 'Deseja mesmo apagar essa atividade e todos os seus dados ?',
+                          title: 'Aviso',
+                          confirmLabel: 'Apagar',
+                          cancelLabel: 'Cancelar',
+                          onConfirm: () {
+                            widget.taskStore.deleteTask(task);
+                            context.popView(context);
+                          },
+                          onCancel: () => context.popView(context),
+                        ),
+                      );
+                    },
+                  )
+                : Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        left: 24,
+                        right: 24,
+                        bottom: kToolbarHeight,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        spacing: 8,
+                        children: [
+                          Text(
+                            'Nada por aqui...',
+                            style: context.headlineLarge?.copyWith(
+                              fontSize: 32,
+                              color: context.primaryColor,
+                              fontWeight: FontWeight.w500,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          FilledButton.icon(
+                            onPressed: () {
+                              context.pushView(
+                                (context) => TaskFormView(
+                                  taskStore: widget.taskStore,
+                                ),
+                              );
+                            },
+                            icon: const Icon(Icons.add),
+                            label: const Text('Nova Tarefa'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+          );
+        }
+
+        return Scaffold(
+          appBar: appBar,
+          body: body,
+          floatingActionButton: floatingActionButton,
         );
       },
     );
